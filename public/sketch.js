08fd919b73892310d46335c8c4d62c0774a3fa83
bsack <bsack23@gmail.com>
2023-07-07 09:53:46 -0400

    make client work on mobile

--- public/sketch.js
+++ public/sketch.js
@@ -9,26 +9,29 @@ if (window.location.hostname == 'localhost') {
     currUrl = window.location.href;
 }
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(windowWidth, windowHeight);
     background(21);
     textAlign(CENTER, CENTER);
     socket = io.connect(currUrl);
     print('connected at: ' + currUrl)
     socket.on('mouse', (data) => {
         fill(255, 0, 100);
+        noStroke();
         ellipse(data.x, data.y, 50);
         fill(0);
+        stroke(0);
         text(data.n, data.x, data.y);
     });
 
     inputBox = createInput('add your name');
     inputBox.mousePressed(() => inputBox.value(''));
     inputBox.input(grabString);
+    inputBox.position(30, 30);
 }
 
 function grabString() {
     inputString = this.value();
-    print(inputString);
+    // print(inputString);
 }
 
 function keyPressed() {
@@ -47,8 +50,10 @@ function keyPressed() {
 
 function mouseDragged() {
     // background(21);
+    noStroke();
     fill(100, 0, 255);
     ellipse(mouseX, mouseY, 50);
+    stroke(0);
     fill(0);
     text(yrname, mouseX, mouseY);
     let data = {
@@ -60,6 +65,11 @@ function mouseDragged() {
     //console.log('Sending: ' + data.x+','+data.y+','+data.n);
 }
 
+
 function draw() {
     // nothing to loop?
 }
+
+document.ontouchmove = function(event) {
+    event.preventDefault();
+};
